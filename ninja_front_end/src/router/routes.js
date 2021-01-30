845a1788cb53080793cb85848b981041745e716f
atulatulatul <atukumar2316singh@gmail.com>
2021-01-30 11:04:49 +0530

    Made the leaderboard page as the landing page.

--- ninja_front_end/src/router/routes.js
+++ ninja_front_end/src/router/routes.js
@@ -1,14 +1,14 @@
 
 const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('pages/Index.vue') }
+  //   ]
+  // },
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  },
-  {
-    path: '/LeaderBoard',
     component: () => import('pages/LeaderBoard.vue'),
   },
 
